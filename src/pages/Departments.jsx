d6baf32e022089467d9e3966540b0d5522a623fd
praveen kumar <praveenshakya1@gmail.com>
2023-08-12 21:52:44 +0530

    commit

--- src/pages/Departments.jsx
+++ src/pages/Departments.jsx
@@ -1,22 +1,34 @@
-import React from "react";
+import React, {useContext} from "react";
 import Dashboard from "../components/Dashboard";
 import { useNavigate } from "react-router-dom";
 import "./department.scss";
+import { DataContext } from "../context/DataContext";
+import { type } from "@testing-library/user-event/dist/type";
 const Departments = () => {
+  const {DataDispatch} = useContext(DataContext);
   const navigate = useNavigate();
-  
+
+  const handleDepartment = (value)=>{
+    console.log(value);
+    DataDispatch({
+      type:"changeDepartment",
+      payload: value,
+    })
+    navigate('/products')
+  }
+
   return (
     <div className="department">
       <Dashboard />
       <div className="dept-container">
         <div className="main-container">
-          <div className="kitchen">
+          <div className="kitchen"  onClick={()=>handleDepartment("Kitchen")}>
             <p>Kitchen</p>
           </div>
-          <div className="clothing">
+          <div className="clothing" onClick={()=>handleDepartment("Clothing")}>
             <p>Clothing</p>
           </div>
-          <div className="toys">
+          <div className="toys" onClick={()=> handleDepartment("Toys")}>
             <p>Toys</p>
           </div>
         </div>
